d60650866d76dab993035e29fe365ca8d7de21ba
Rabie TAWIL <rabietawil@gmail.com>
2019-06-12 11:20:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+
+const var1 = 31;
+const var2 = 2;
+const multiply = var1 * var2;
+
+const random = Math.random();
+
+const mod = 18 % 7;
+
+const max = Math.max(2,5,10,15,20);
