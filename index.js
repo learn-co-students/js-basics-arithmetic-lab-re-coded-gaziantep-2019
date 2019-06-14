8a4c9de67c5495fdb735838ea6bd0c9b52bec0eb
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-06-14 13:10:58 +0000

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let var1 = 31;
 let var2 = 2;
 let multiply = var1 * var2;
 
-let random = "";
+let random;
 random = Math.ceil(Math.random() +1);
 
-let mod = "";
+let mod;
 mod = 31 % 9;
 
-let max = "";
+let max;
 max = Math.max(5,10,15,20);
