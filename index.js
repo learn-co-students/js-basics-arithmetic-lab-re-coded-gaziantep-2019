9457e4891fc897f1a6c49833f52225de0f245f31
kassem241 <34236354+kassem241@users.noreply.github.com>
2019-07-03 18:17:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code here
-const var1 = 31;
-const var2 = 2;
-const multiply = var1 * var2;
-const random = Math.random();
-const mod = 18 % 7;
-const max = Math.max(2,5,10,15,20);
+let var1 = 31;
+let var2 = 2;
+let multiply = var1 * var2;
+let random = Math.random();
+let mod = 18 % 7;
+let max = Math.max(2,5,10,15,20);
