9c476796b9686ed774c8fc8bc61e7351530dbf47
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-03 07:02:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here
+
+const var1 = 31;
+const var2 = 2 ;
+
+let multiply = var1 * var2 ;
+
+let random = Math.floor(Math.random()*10);
+
+let mod =29;
+
+mod %= 5;
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,20);
