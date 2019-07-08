45ffe39e766c9e34d460ca5b48f110ed96c50ab6
nour-alhuda <nouralasali@hotmail.com>
2019-07-08 23:00:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here
+let var1 = 31;
+let var2 = 2;
+let multiply = var1 * var2;
+let random = Math.floor(Math.random()*10) + 1;
+let mod = 19 % 5; 
+let max = Math.max(2,4,20,7);
