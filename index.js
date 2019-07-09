a43dfcf20b4b9cdde7b622cb49a6a32b8c5ef64f
Razan Hajja <razan.alhajjeh@hotmail.com>
2019-07-09 18:17:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+let var1 = 2;
+let var2 = 31;
+let multiply = var1 * var2 ;
+Math.floor(Math.random() * 10) + 1;
+let var3 =10;
+let var4 =6;
+let mod = var3 % var4 ;
+let max=20 ;
+Math.max(1, 20, 3, 4, 5);
+let random  =1;
+Math.floor(Math.random() * 10) + 1;
