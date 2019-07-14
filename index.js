17e5370c39b4a633de24d728d759597c076953a4
Halil güler <h.guler3717@gmail.com>
2019-07-14 14:13:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code here
+
+let var1 = 31; 
+let var2 = 2;
+let multiply = var1 * var2;
+
+let random = Math.ceil(Math.random()*10);
+
+let mod = 14 % 10;
+
+let max = Math.max(15,16,20)
+/*describe('index.js', function() {
+  describe('multiply', function() {
+    it("is an equation whose multiplied results will equal 62", function() {
+      expect(var1).to.be.a('number')
+      expect(var2).to.be.a('number')
+      expect(multiply).to.eq(62)
+    });
+  });
+  describe('random', function() {
+    it("is generates a random integer greater than 0", function() {
+      expect(random).to.be.a('number')
+      expect(random).to.not.eq(0)
+    });
+  });
+  describe('mod', function() {
+    it("is generates a remainder that is equal to 4", function() {
+      expect(mod).to.eq(4)
+    });
+  });
+  describe('max', function() {
+    it("will return 20 as the highest number in the set", function() {
+      expect(max).to.eq(20)
+    });*/
