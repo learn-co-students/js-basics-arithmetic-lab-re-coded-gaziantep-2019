2d4a5d501bb03f8d7c4a02ca98409dac13066bea
Ahmed <a.gado.2df@gmail.com>
2019-07-18 23:38:31 +0300

    cool Project

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here
+let var1 = 2;
+let var2 = 31;
+let multiply = var1 * var2;
+let random = Math.floor(Math.random()*100)+1;
+let mod = 178%6
+let max = Math.max(20, 3, 5, 13);
